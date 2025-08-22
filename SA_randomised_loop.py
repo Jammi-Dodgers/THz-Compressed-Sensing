94d240632529fc0942a5cbc0bc27e1708075ed34
Watts <Jamie.Watts1@stfc.ac.uk>
2025-08-22 16:08:38 +0100

    updated graphs but data is too noisy. 0.5 pixel pitch might be too ambitious.

--- SA_randomised_loop.py
+++ SA_randomised_loop.py
@@ -4,11 +4,11 @@ import CS_functions as cs
 
 os.chdir(os.path.dirname(__file__))
 
-file_number = 27
+file_number = 29
 file_name = "1dmockanderrors{:d}".format(file_number)
 file_type = ".csv"
 
-optlocs_file = r"data/1dmockanderrors27_randoptlocs.csv"
+optlocs_file = r"data/1dmockanderrors29_randoptlocs.csv"
 
 target, target_err = cs.open_dataset(file_name, ".csv")
 
@@ -19,7 +19,12 @@ interferogram_length = len(target)
 
 for n in range(iterations):
 
-    detectors, score = cs.simulated_annealing(number_of_sensors, target, np.ones_like(target_err), regularization_coeffient =1e-3, subsampling_method= "random", iterations= 5000, max_temp= 17)
+    detectors, score = cs.simulated_annealing(number_of_sensors, target, np.ones_like(target_err),
+                                              regularization_coeffient =1e-3,
+                                              subsampling_method= "random",
+                                              iterations= 5000,
+                                              max_temp= 31,
+                                              cooling= 0.995)
 
     cs.append_array_to_csv(detectors, optlocs_file)
 
